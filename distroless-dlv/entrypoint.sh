00cf77dbffcdda149473c081c8c139bccc1be893
Alex Babel <alex@alexbabel.com>
2025-11-14 13:09:29 +0100

    fix(distroless-dlv): add missing binaries for tilt

--- distroless-dlv/entrypoint.sh
+++ distroless-dlv/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ "${CONTAINER_ROLE}" = "init" ]; then
-  CLI_BIN=${CLI_BIN:-/bin/cli}
-  $CLI_BIN migrate up "$@"
+  INIT_CMD=${INIT_CMD:-"/bin/cli migrate up"}
+  $INIT_CMD "$@"
 elif [ "${CONTAINER_ROLE}" = "worker" ]; then
-  WORKER_BIN=${WORKER_BIN:-/bin/worker}
-  dlv exec "$WORKER_BIN" --wd=/ --headless --listen=:2345 --accept-multiclient --continue  "$@"
+  WORKER_CMD=${WORKER_CMD:-/bin/worker}
+  dlv exec "$WORKER_CMD" --wd=/ --headless --listen=:2345 --accept-multiclient --continue  "$@"
 else
-  MAIN_BIN=${MAIN_BIN:-/bin/main}
-  dlv exec "$MAIN_BIN" --wd=/ --headless --listen=:2345 --accept-multiclient --continue  "$@"
+  MAIN_CMD=${MAIN_CMD:-/bin/main}
+  dlv exec "$MAIN_CMD" --wd=/ --headless --listen=:2345 --accept-multiclient --continue  "$@"
 fi
