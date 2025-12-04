bbdfb9e1c7532e189ad9645f6c6c2e6fbcbf2e1a
Alex Babel <alex@alexbabel.com>
2025-12-04 10:49:21 +0100

    fix(distroless-dlv): add support for global binary

--- distroless-dlv/entrypoint.sh
+++ distroless-dlv/entrypoint.sh
@@ -4,8 +4,11 @@ if [ "${CONTAINER_ROLE}" = "init" ]; then
   $INIT_CMD "$@"
 elif [ "${CONTAINER_ROLE}" = "worker" ]; then
   WORKER_CMD=${WORKER_CMD:-/bin/worker}
-  dlv exec "$WORKER_CMD" --wd=/ --headless --listen=:2345 --accept-multiclient --continue  "$@"
+  dlv exec "$WORKER_CMD" --wd=/ --headless --listen=:2345 --accept-multiclient --continue -- "$@"
+elif [ "${CONTAINER_ROLE}" = "global" ]; then
+  GLOBAL_CMD=${GLOBAL_CMD:-/bin/global}
+  dlv exec "$GLOBAL_CMD" --wd=/ --headless --listen=:2345 --accept-multiclient --continue -- "$@"
 else
   MAIN_CMD=${MAIN_CMD:-/bin/main}
-  dlv exec "$MAIN_CMD" --wd=/ --headless --listen=:2345 --accept-multiclient --continue  "$@"
+  dlv exec "$MAIN_CMD" --wd=/ --headless --listen=:2345 --accept-multiclient --continue -- "$@"
 fi
